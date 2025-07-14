7ea6b82fb694b4ca1851aa2475c58aa2cdd2a2ff
rajneesh <rajneeshkumar9674737@gmail.com>
2025-07-14 13:57:33 +0530

    added:CORS

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from flasgger import Swagger, LazyJSONEncoder
 from routes.dataset_routes import dataset_bp
 from routes.home_routes import home_bp
@@ -7,6 +8,11 @@ from routes.quality_routes import quality_bp
 
 app = Flask(__name__)
 app.json_encoder = LazyJSONEncoder
+
+cors_origins = os.getenv("CORS_ORIGINS", "")
+allowed_origins = [origin.strip() for origin in cors_origins.split(",") if origin.strip()]
+
+CORS(app, resources={r"/*": {"origins": allowed_origins}})
 
 template = {
     "swagger": "2.0",
@@ -28,11 +34,11 @@ app.register_blueprint(quality_bp)
 
 # for development used this method-----------
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+
+
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
 
