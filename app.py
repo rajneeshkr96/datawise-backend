27e36cad2580436d53c869be47127a72d74cb513
rajneesh <rajneeshkumar9674737@gmail.com>
2025-07-14 14:45:25 +0530

    fix:cors error

--- app.py
+++ app.py
@@ -2,19 +2,25 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flasgger import Swagger, LazyJSONEncoder
+from dotenv import load_dotenv
+
 from routes.dataset_routes import dataset_bp
 from routes.home_routes import home_bp
 from routes.quality_routes import quality_bp
 
+load_dotenv()
+
 app = Flask(__name__)
 app.json_encoder = LazyJSONEncoder
 
-# cors_origins = os.getenv("CORS_ORIGINS", "")
-# allowed_origins = [origin.strip() for origin in cors_origins.split(",") if origin.strip()]
+cors_origins = os.getenv("CORS_ORIGINS", "*")
 
-# CORS(app, resources={r"/*": {"origins": allowed_origins}})
+if cors_origins.strip() == "*":
+    CORS(app)  
+else:
+    allowed_origins = [origin.strip() for origin in cors_origins.split(",") if origin.strip()]
+    CORS(app, resources={r"/*": {"origins": allowed_origins}})
 
-CORS(app)
 
 template = {
     "swagger": "2.0",
@@ -26,21 +32,13 @@ template = {
     "basePath": "/",
     "schemes": ["http"]
 }
-
 Swagger(app, template=template)
 
-# Register routes
 app.register_blueprint(dataset_bp)
 app.register_blueprint(home_bp)
 app.register_blueprint(quality_bp)
 
-# for development used this method-----------
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
